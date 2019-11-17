7aa665769fe4c4a8ecb4c9b5395183d75d1c08d8
Tami Zwick <tami.zwick@yahoo.com>
2019-11-16 20:59:15 -0600

    added instructions if no ingredients in state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,23 +1,22 @@
-import React, {Component} from 'react';
-import Aux from '../../hoc/Auxiliary';
+import React, {Component, Fragment} from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 1,
-            bacon: 1,
+            bacon: 3,
             cheese: 2,
-            meat: 2
+            meat: 1
         }
     }
 
     render() {
         return (
-            <Aux>
+            <Fragment>
                 <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
-            </Aux>
+            </Fragment>
         );
     }
 }
